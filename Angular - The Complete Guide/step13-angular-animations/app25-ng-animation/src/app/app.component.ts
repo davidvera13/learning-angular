85b26e09064748fa6025551fc63a6d591ae3edcb
davidvera13 <david.vera@9online.fr>
2023-06-13 00:04:01 +0200

    feat(animation): project startup

---
+++ Angular - The Complete Guide/step13-angular-animations/app25-ng-animation/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app25-ng-animation';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item: string) {
+    this.list.push(item);
+  }
+
+  onDelete(item: string) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+}
