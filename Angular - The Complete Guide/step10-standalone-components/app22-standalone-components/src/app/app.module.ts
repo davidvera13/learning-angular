e9cc60987d8a197d29dbe78d5fac2e17f023daa5
davidvera13 <david.vera@9online.fr>
2023-06-12 20:32:26 +0200

    feat(standalone): standalone component startup project

---
+++ Angular - The Complete Guide/step10-standalone-components/app22-standalone-components/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { DetailsComponent } from './components/welcome/details/details.component';
+import {SharedModule} from "./shared/shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
