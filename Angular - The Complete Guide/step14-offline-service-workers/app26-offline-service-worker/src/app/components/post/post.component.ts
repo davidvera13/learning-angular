5bd5b8f68549a8424853667c740c7f59a8ad92e6
davidvera13 <david.vera@9online.fr>
2023-06-13 21:21:11 +0200

    feat(pwa - service workers): project setup with pwa added

---
+++ Angular - The Complete Guide/step14-offline-service-workers/app26-offline-service-worker/src/app/components/post/post.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+  @Input() title: string;
+  @Input() content: string;
+
+  constructor() {
+    this.title = '';
+    this.content = '';
+  }
+
+}
