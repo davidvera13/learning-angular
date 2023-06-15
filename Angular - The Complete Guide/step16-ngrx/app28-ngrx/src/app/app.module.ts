875bc590014ef583d2e57992bb6345272ad1b001
davidvera13 <david.vera@9online.fr>
2023-06-15 07:18:07 +0200

    feat(ngrx): project startup

---
+++ Angular - The Complete Guide/step16-ngrx/app28-ngrx/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterControlsComponent } from './components/counter-controls/counter-controls.component';
+import { CounterOutputComponent } from './components/counter-output/counter-output.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterControlsComponent,
+    CounterOutputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
